289ba171aa2154e23a83c471d0ec52ea31e539f8
brycegough <bryce@freeform.com.au>
2021-07-15 14:59:35 +0800

    Update api.php

--- api.php
+++ api.php
@@ -7,49 +7,52 @@ use Auth0\SDK\Helpers\Tokens\TokenVerifier;
 $app->on('cockpit.api.authenticate', function($data) use($app) {
 
     if ( $data['token'] ) {
-
+        
         if ($data['resource'] == 'auth0') {
             $user = $this->module('auth0')->userinfo($data['token'], [
                 'normalize' => true,
                 'cache'     => $app->retrieve('config/auth0/cache', false)
             ]);
-
+    
             if ($user) {
                 $data['authenticated'] = true;
                 $data['user'] = $user;
             }
-
+            
         } else {
-
-            // Authenticate token scope
-            // TODO - cache & scopes
-
+            
+            // Authenticate token scope 
+            // TODO - cache
+            
             try {
                 $accessToken = $data['token'];
-
+                
                 $issuer = 'https://' . $app->module('auth0')->getDomain() . '/';
                 $audience = $app->module('auth0')->getAudience();
-
+                
                 $jwksFetcher = new JWKFetcher();
                 $jwks        = $jwksFetcher->getKeys($issuer . '.well-known/jwks.json');
                 $sigVerifier = new AsymmetricVerifier( $jwks );
-
+                
                 $tokenVerifier = new TokenVerifier($issuer, $audience, $sigVerifier);
                 $decoded = $tokenVerifier->verify( $accessToken );
-
-                // Allow access via API - TODO scopes
-
+                
                 if ( $decoded ) {
+                    
+                    $permissions = $decoded['permissions'];
+                    
+                    // TODO - permissions
+                
                     $data['authenticated'] = true;
                 }
-
+                
             } catch (\Exception $e) {
                 $this->stop([ 'error' => $e->getMessage() ], 401);
             }
-
+            
         }
     }
-
+    
 });
 
 $app->on('cockpit.rest.init', function($routes) use($app) {
